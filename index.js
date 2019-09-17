f3ec9cdc44684b77e40366980a8ba7be48d0ca3e
McPopato <55455377+McPopato@users.noreply.github.com>
2019-09-17 20:09:58 +0300

    Fixed some bugs

--- index.js
+++ index.js
@@ -1,21 +1,26 @@
-// when a chat event is received
 var result;
 var dice;
 var sides;
 var i;
 var total;
 var content;
-var numbers;
+var numbers = [1,6];
 var roll;
 var sender;
 global.Brikkit.on('chat', evt => {
 		content = evt.getContent();
 		sender = evt.getSender().getUsername();
-		if(content.includes('!diceroll')) {
+		if(content.includes('!diceroll') & /\d/.test(content) == true) {
 			numbers = content.match(/\d+/g).map(Number);
 			result = 0;
 			dice = numbers[0];
 			sides = numbers[1];
+			if(dice > 100) {
+				dice = 100;
+			}
+			if(sides > 1000) {
+				sides = 1000;
+			}
 			for (i = 0; i < dice;i++) {
 				roll = Math.floor(Math.random() * sides) + 1;
 				if(roll > sides) {
